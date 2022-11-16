856ca5904e1056971a68453d6ed702b0c37afc60
Jurabek <80104207+Tojiddinov@users.noreply.github.com>
2022-11-16 13:15:46 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,65 @@
+let navbar = document.querySelector('.header .flex .navbar');
 
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let videoBtn = document.querySelectorAll('.vid-btn');
-
-window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
 }
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-});
+let account = document.querySelector('.user-account');
 
-searchBtn.addEventListener('click', () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');
-});
+document.querySelector('#user-btn').onclick = () =>{
+   account.classList.add('active');
+}
 
-formBtn.addEventListener('click', () =>{
-    loginForm.classList.add('active');
-});
+document.querySelector('#close-account').onclick = () =>{
+   account.classList.remove('active');
+}
 
-formClose.addEventListener('click', () =>{
-    loginForm.classList.remove('active');
-});
+let myOrders = document.querySelector('.my-orders');
 
-videoBtn.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
-        document.querySelector('.controls .active').classList.remove('active');
-        btn.classList.add('active');
-        let src = btn.getAttribute('data-src');
-        document.querySelector('#video-slider').src = src;
-    });
-});
+document.querySelector('#order-btn').onclick = () =>{
+   myOrders.classList.add('active');
+}
 
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-    },
-});
+document.querySelector('#close-orders').onclick = () =>{
+   myOrders.classList.remove('active');
+}
 
-var swiper = new Swiper(".brand-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        450: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1200: {
-          slidesPerView: 5,
-        },
-      },
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+   cart.classList.add('active');
+}
+
+document.querySelector('#close-cart').onclick = () =>{
+   cart.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   myOrders.classList.remove('active');
+   cart.classList.remove('active');
+};
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next(){
+   slides[index].classList.remove('active');
+   index = (index + 1) % slides.length;
+   slides[index].classList.add('active');
+}
+
+function prev(){
+   slides[index].classList.remove('active');
+   index = (index - 1 + slides.length) % slides.length;
+   slides[index].classList.add('active');
+}
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+   acco.onclick = () =>{
+      accordion.forEach(remove => remove.classList.remove('active'));
+      acco.classList.add('active');
+   }
 });
